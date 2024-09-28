094b29fdd07531e4e9c3070f559ad1606418befe
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-09-28 11:55:52 +0200

    Version initiale

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<title>Jeu de 2048</title>
+
+<h1>Ceci est un jeu de 2048</h1>
+
+<style>
+    /* Importer la police Roboto depuis Google Fonts*/
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'); 
+</style>
+
